798c0aa9630b42dfeac004aeafb749de2f1f7174
igor <igor5430@mail.com>
2021-03-23 21:46:11 +0300

    second commit

--- src/test/java/DemoQATests.java
+++ src/test/java/DemoQATests.java
@@ -12,35 +12,42 @@ public class DemoQATests {
 
     @Test
     void practiceFormTest() {
-        Configuration.timeout = 4000;
+        //Configuration.timeout = 4000;
+        String firstName = "Gleb";
+        String lastName = "Hleb";
+        String email = lastName+"@null.ru";
+        String userNumber = "9052741001";
+        String gender = "Male";
+        String currentAddress = "Urupinsk";
+
         open("https://demoqa.com/automation-practice-form");
 
-        $("#firstName").setValue("Gleb");
-        $("#lastName").setValue("Hleb");
-        $("#userEmail").setValue("Hleb@null.ru");
-        $(byText("Male")).click();
-        $("#userNumber").setValue("9052741001");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(byXpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[1]/form[1]/div[5]/div[2]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/select[1]")).selectOption("December");
-        $(byXpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[1]/form[1]/div[5]/div[2]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/select[1]")).selectOption("1991");
-        $(byXpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[1]/form[1]/div[5]/div[2]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]")).click();
+        $(".react-datepicker__month-select").selectOption("December");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $(byXpath("(//div[contains(text(),'2')])[1]")).click();
         $("#subjectsInput").setValue("Arts").pressEnter();
         $(byText("Music")).click();
-        $("#uploadPicture").uploadFile(new File("src\\test\\java\\1.jpg"));
-        $("#currentAddress").setValue("Urupinsk");
+        $("#uploadPicture").uploadFile(new File("src\\test\\java\\resources\\1.jpg"));
+        $("#currentAddress").setValue(currentAddress);
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
         $("#submit").scrollTo().click();
         //осетр
-        $(byXpath("//tbody/tr[1]/td[2]")).shouldHave(text("Gleb Hleb"));
-        $(byXpath("//tbody/tr[2]/td[2]")).shouldHave(text("Hleb@null.ru"));
-        $(byXpath("//tbody/tr[3]/td[2]")).shouldHave(text("Male"));
-        $(byXpath("//tbody/tr[4]/td[2]")).shouldHave(text("9052741001"));
+        $(byXpath("//tbody/tr[1]/td[2]")).shouldHave(text(firstName+" "+lastName));
+        $(byXpath("//tbody/tr[2]/td[2]")).shouldHave(text(email));
+        $(byXpath("//tbody/tr[3]/td[2]")).shouldHave(text(gender));
+        $(byXpath("//tbody/tr[4]/td[2]")).shouldHave(text(userNumber));
         $(byXpath("//tbody/tr[5]/td[2]")).shouldHave(text("02 December,1991"));
         $(byXpath("//tbody/tr[6]/td[2]")).shouldHave(text("Arts"));
         $(byXpath("//tbody/tr[7]/td[2]")).shouldHave(text("Music"));
         $(byXpath("//tbody/tr[8]/td[2]")).shouldHave(text("1.jpg"));
-        $(byXpath("//tbody/tr[9]/td[2]")).shouldHave(text("Urupinsk"));
+        $(byXpath("//tbody/tr[9]/td[2]")).shouldHave(text(currentAddress));
         $(byXpath("//tbody/tr[10]/td[2]")).shouldHave(text("NCR Delhi"));
 
     }
